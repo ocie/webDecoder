600cfa39893918101530804706c05e3bef40cde3
Ocie Mitchell <ocie_mitchell@yahoo.com>
2024-07-12 20:57:34 -0700

    finish webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/background/background.js',
+  entry: {
+    background: {
+      import: './src/background/background.js',
+      filename: 'background.js'
+    },
+    content: {
+      import: './src/content/content-script.js',
+      filename: 'content-script.js'
+    }
+  },
   output: {
-    filename: 'background.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/popup/popup.html", to: path.resolve(__dirname, 'dist') },
+        { from: "**/*", context: path.resolve(__dirname, "src", "assets"), to: path.resolve(__dirname, 'dist') },
+        { from: "./src/content/content-script.css", to: path.resolve(__dirname, 'dist')}
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ]
 };
