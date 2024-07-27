16b79d47fa5ab0e21e586a79f2fe34fdc1d3771e
Ocie Mitchell <ocie_mitchell@yahoo.com>
2024-07-27 16:04:06 -0700

    add scrollbar

--- src/content/content-script.ts
+++ src/content/content-script.ts
@@ -1,27 +1,33 @@
-const dialog = document.createElement('dialog');
-dialog.id = 'decoder-base';
-
-document.body.appendChild(dialog);
-
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    const newChildren=[];
+    const dialog = document.createElement('dialog');
+    dialog.className = 'decoder-base';
+    document.body.appendChild(dialog);
+    
+    const header = document.createElement('div');
+    header.className = 'decoder-head';
+    dialog.appendChild(header);
+
+    const body = document.createElement('div');
+    body.className = 'decoder-content';
+    dialog.appendChild(body);
 
     const close = document.createElement('button');
     close.onclick = (e) => {e.preventDefault(); dialog.close();}
     close.innerText = '[X]';
-    newChildren.push(close);
-    const p = document.createElement('p');
-    newChildren.push(p);
+    header.appendChild(close);
+
     const maxLength=64;
-    const selection = request.text.substring(0,64);
     const elipsis = request.text.length > maxLength;
-    p.innerText = 'You selected ' + request.text.substring(0,64) + (elipsis?'...':'');
+    const p = document.createElement('p');
+    p.innerText = 'You selected ' + request.text.substring(0,maxLength) + (elipsis?'...':'');
+    header.appendChild(p);
+
+    const newChildren=[];
     for (const decoding of request.decodings) {
         const sub = document.createElement('p');
         newChildren.push(sub);
         sub.innerText = `${decoding.name}: ${decoding.text}`;
     }
-    dialog.replaceChildren(...newChildren);
+    body.replaceChildren(...newChildren);
     dialog.show();
 });
